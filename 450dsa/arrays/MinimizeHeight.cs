db6a008d91c25e2a7728c9ac0cf06e7bcd222578
Tarun Sahu <tk.sahu@hotmail.com>
2021-05-17 22:00:00 +0530

    added few more gkg problems

---
+++ 450dsa/arrays/MinimizeHeight.cs
@@ -0,0 +1,22 @@
+﻿using algo.problems.common;
+
+namespace algo.problems._450dsa.arrays
+{
+    /// <summary>
+    /// https://practice.geeksforgeeks.org/problems/minimize-the-heights3351/1
+    /// </summary>
+    public class MinimizeHeight : IProblem
+    {
+        public void Run()
+        {
+            var input = new[] {3, 9, 12, 16, 20};
+            Minimize(input);
+            input.Dump();
+        }
+
+        private void Minimize(int[] input)
+        {
+            
+        }
+    }
+}
